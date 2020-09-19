4de03ba1e5d4f74960542db2ebf04458f247c169
abish373 <abishp3@gmail.com>
2020-09-20 00:44:18 +0530

    bug-fixed

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -9,9 +9,9 @@ function ContactForm() {
             <div className="form-wrapper">
                 <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
                     <input type="hidden" name="form-name" value="contact" />
-                    <input type="text" placeholder="Enter your Name(optional)" />
-                    <input type="email" placeholder="Enter your Email(optional)" />
-                    <textarea placeholder="Write something to me">
+                    <input type="text" name="name" placeholder="Enter your Name(optional)" />
+                    <input type="email" name="email" placeholder="Enter your Email(optional)" />
+                    <textarea name="text" placeholder="Write something to me">
 
                     </textarea>
                     <button type="submit">Send</button>
