caf7576d27c56f4a0e7338e303de15e74be9c86e
abish373 <abishp3@gmail.com>
2020-09-20 17:22:16 +0530

    form fixed

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -45,8 +45,8 @@ function ContactForm() {
             <div className="form-wrapper">
                 <form className="form" name="contact" action="/contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
                     <input type="hidden" name="form-name" value="contact" />
-                    <input type="text" name="name" placeholder="Enter your Name(optional)" />
-                    <input type="email" name="email" placeholder="Enter your Email(optional)" />
+                    <input type="text" name="name" placeholder="Enter your Name" />
+                    <input type="email" name="email" placeholder="Enter your Email" />
                     <textarea name="text" placeholder="Write something to me">
 
                     </textarea>
