ca9e8512aefd0a37d2a205541a01201f4341a355
abish373 <abishp3@gmail.com>
2020-09-20 16:03:23 +0530

    form bug fixed

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,24 +1,61 @@
 import React from 'react'
 import NavBar from '../menu/NavBar'
+import NetlifyForm from 'react-netlify-form'
 import './contactform.css'
 
 function ContactForm() {
+
+
     return (
         <div className="contact-form">
             <NavBar />
-            <div className="form-wrapper">
-                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                    <input type="hidden" name="form-name" value="contact" />
-                    <input type="text" name="name" placeholder="Enter your Name(optional)" />
-                    <input type="email" name="email" placeholder="Enter your Email(optional)" />
-                    <textarea name="text" placeholder="Write something to me">
+        <div className="form-wrapper">
+        <NetlifyForm name='Contact Form'>
+            {({ loading, error, success }) => (
+                <div>
+                    {loading &&
+                        <div>Loading...</div>
+                    }
+                    {error &&
+                        <div>Your information was not sent. Please try again later.</div>
+                    }
+                    {success &&
+                        <div>Thank you for contacting us!</div>
+                    }
+                    {!loading && !success &&
+                        <div className="form">
+                            <input type="hidden" name="form-name" value="contact" />
+                            <input type="text" name="name" placeholder="Enter your Name(optional)" />
+                            <input type="email" name="email" placeholder="Enter your Email(optional)" />
+                            <textarea name="text" placeholder="Write something to me">
 
-                    </textarea>
-                    <button type="submit">Send</button>
-                </form>
-            </div>
+                            </textarea>
+                            <button type="submit">Send</button>
+                        </div>
+                    }
+                </div>
+            )}
+        </NetlifyForm>
+        </div>
         </div>
     )
+
+    // return (
+    //     <div className="contact-form">
+    //         <NavBar />
+    //         <div className="form-wrapper">
+    //             <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+    //                 <input type="hidden" name="form-name" value="contact" />
+    //                 <input type="text" name="name" placeholder="Enter your Name(optional)" />
+    //                 <input type="email" name="email" placeholder="Enter your Email(optional)" />
+    //                 <textarea name="text" placeholder="Write something to me">
+
+    //                 </textarea>
+    //                 <button type="submit">Send</button>
+    //             </form>
+    //         </div>
+    //     </div>
+    // )
 }
 
 export default ContactForm
