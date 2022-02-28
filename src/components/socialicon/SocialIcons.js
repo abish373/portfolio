4fa2d6d0d1e97517475f32962476ee44d10167d5
Abish <53331748+abish373@users.noreply.github.com>
2022-02-28 14:50:12 +0530

    linkedin url update

--- src/components/socialicon/SocialIcons.js
+++ src/components/socialicon/SocialIcons.js
@@ -15,7 +15,7 @@ function SocialIcons() {
             <div className={ (winWidth < 500 ? "icon-wrapper-mob" : 'icon-wrapper')}>
                 <ul className={winWidth < 500 ? "icon-list-mob" : 'icon-list'}>  
                     <Item link="https://github.com/abish373/" imgPath='./assets/github.svg' />
-                    <Item link="https://www.linkedin.com/in/abish-abish-579b7314a/" imgPath='./assets/linkedin.svg'/>
+                    <Item link="https://www.linkedin.com/in/abish-p/" imgPath='./assets/linkedin.svg'/>
                     <Item link="https://www.instagram.com/abish373/" imgPath='./assets/instagram.svg' />
                     <Item link="https://www.facebook.com/abish.abish.547" imgPath='./assets/facebook.svg'/>
                 </ul>
